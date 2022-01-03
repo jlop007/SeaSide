c72c87ec7f44e9839012c7562825af37c118c149
Julio <j-l20@live.com>
2022-01-03 07:57:46 -0500

    Add files via upload

---
+++ SeaSide/js/main.js
@@ -0,0 +1,13 @@
+// Global variables
+
+
+// Toggle navigation
+
+
+//Add/remove header border bottom on scroll
+
+
+// Add selected link styles to the link clicked and close navigation when clicking the links
+
+
+// Scroll Reveal
